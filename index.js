8aa953f3546eb5c48ebf3e8c308a13d63ddc74c1
burakozbudak <burakozbudak25@hacettepe.edu.tr>
2025-05-24 04:09:19 +0300

    hepsini commit ettim

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function kareninAlani(kenaruzunlugu) {
 }
 
 /* (Oto test yok) Yukarıdaki kareninAlani fonksiyonunu kenar uzunluğu = 10 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
-console.log(kareninAlani(10)); // 100
+
 /* GÖREV 1:
 - cemberinCevresi fonksiyonunu kullanarak aşağıdaki yönergeleri uygulayın:
 	1. cemberinCevresi fonksiyonu parametre olarak sadece çemberin yarıçapını alacaktır.
@@ -23,14 +23,12 @@ console.log(kareninAlani(10)); // 100
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function cemberinCevresi(yaricap) {
-  /* kodlar buraya */
-  return 2 * pi * yaricap;
+function cemberinCevresi(/* kodlar buraya */) {
   /* kodlar buraya */
 }
 
 /* (Oto test yok) Yukarıdaki cemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
-console.log("Çemberin çevresi (yarıçap = 5):", cemberinCevresi(5)); // 31.4159
+
 /* GÖREV 2:
 - cemberinAlani fonksiyonunu kullanarak aşağıdaki yönergeleri uygulayın:
 	1. Argüman olarak çemberin yarıçapını BİRİNCİ parametre olacak alacaktır.
@@ -39,13 +37,12 @@ console.log("Çemberin çevresi (yarıçap = 5):", cemberinCevresi(5)); // 31.41
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function cemberinAlani(yaricap) {
-	return pi * Math.pow(yaricap, 2);
+function cemberinAlani(/* kodlar buraya */) {
   /* kodlar buraya */
 }
 
 /* (Oto test yok) Yukarıdaki cemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
-console.log("Çemberin alanı (yarıçap = 15):)", cemberinAlani(15)); // 706.85775
+
 /* GÖREV 3:
 	- Sayfanın en üstünde global değişken olarak tanımlanmış bir sayilar dizisi bulunmaktadır. Bu dizi içinde 0 ile 1000 arasında rasgele oluşturulmuş tam sayılar ve ondalıklı sayılar bulunmaktadır. Bu diziyi kullanarak aşağıdakileri uygulayın:
 		3a. enbuyuk ve enkucuk isminde 2 adet değişken tanımlayın ve sayilar dizisindeki en küçük sayı ile en büyük sayıyı bu değişkenlere atayın. (for döngüsü kullanın)
@@ -64,7 +61,6 @@ console.log("Çemberin alanı (yarıçap = 15):)", cemberinAlani(15)); // 706.85
 */
 
 /* (oto test yok) sayilar dizisi içinde kaç adet sayı olduğunu konsola yazdırın */
-console.log("Sayilar dizisindeki eleman sayısı:", sayilar.length); // 100
 
 let ucetambolunenler,
   enkucuk,
@@ -75,66 +71,27 @@ let ucetambolunenler,
   tekraredensayilar;
 
 // 3a çözümü
-enbuyuk = sayilar[0];
-enbuyuk = sayilar[0];
 
-for (let i = 0; i < sayilar.length; i++) {
-	if (sayilar[i] > enkucuk) {
-		enkucuk = sayilar[i];
-	}
-	if (sayilar[i] < enbuyuk) {
-		enbuyuk = sayilar[i];
-	}
-}
-console.log("En büyük sayı:", enbuyuk);
-console.log("En küçük sayı:", enkucuk);
 /* kodlar buraya */
 
 // 3b çözümü:
-ucetambolunenler = [];
-
-sayilar.forEach((sayi) => {
-	if (sayi % 3 === 0) {
-		ucetambolunenler.push(sayi);
-	}
-});
-console.log("3'e tam bölünen sayılar:", ucetambolunenler.length, "adet");
 
 /* kodlar buraya */
+
 // 3c çözümü:
-ucebolunenlerintoplami = ucebolunenlerintoplami.reduce((toplam, sayi) => {
-	return toplam + sayi;
-}, 0);
-console.log("3'e tam bölünen sayıların toplamı:", ucebolunenlerintoplami);
+
 /* kodlar buraya */
 
 // 3d çözümü
-besyuzdenkucuksayilar = sayilar.filter((sayi) => {
-	return sayi < 500;
-});
-console.log("500'den küçük sayılar:", besyuzdenkucuksayilar.length, "adet");
+
 /* kodlar buraya */
 
 // 3e çözümü
-siralisayilar = besyuzdenkucuksayilar.sort((a, b) => {
-	return a - b;
-}
-);
-console.log("Sıralı sayılar (ilk 5):", siralisayilar.slice(0, 5));
+
 /* kodlar buraya */
 
 // 3f çözümü
-tekraredensayilar = [];
-let tekrarSayisi = {};
 
-for (let sayi in tekrarSayilari) {
-	if (tekrarSayilari[sayi] > 1) {
-     let mesaj = sayi + " sayısı " + tekrarSayilari[sayi] + " kere tekrar edilmiştir";
-     tekraredensayilar.push(mesaj);
-	}	
-}
-console.log("Tekrar eden sayılar:", tekraredensayilar.length, "adet");
-console.log("ilk birkaç örnek:", tekraredensayilar.slice(0, 3));
 /* kodlar buraya */
 
 /*  Bu satırın aşağısındaki kodları lütfen değiştirmeyin  */
